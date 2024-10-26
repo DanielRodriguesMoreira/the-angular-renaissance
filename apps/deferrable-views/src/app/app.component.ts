f261b263391b8ef89829b280e12e5412b7f6e559
drmoreira <drmoreira@outlook.pt>
2024-10-26 20:31:51 +0100

    add deferrable-views app

---
+++ apps/deferrable-views/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: '<h1>Deferrable Views Demo</h1>'
+})
+export class AppComponent {}
